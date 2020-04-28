b6dac47711b9afc1833e6c8d5e8c84ea4ac086ad
Tomas Bedrich <tbedrich@akamai.com>
2020-04-28 12:27:44 +0200

    Use pilot-specific feed

--- monitor.py
+++ monitor.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 import asyncio
-from dataclasses import dataclass
 import datetime
-from email.utils import parsedate_to_datetime
 import logging
+import sys
+from collections import MutableSet
+from dataclasses import dataclass
+from email.utils import parsedate_to_datetime
 from typing import List
 from xml.etree import ElementTree
-from llconfig import Config
 
 from aiohttp import ClientError, ClientSession, ClientTimeout
+from llconfig import Config
 from llconfig.converters import bool_like
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 config = Config()
-config.init("FEED_URL", str, "https://www.xcontest.org/rss/flights/?world/en")
+config.init("FEED_URL", str)
 config.init("HTTP_TIMEOUT", lambda val: ClientTimeout(total=int(val)), ClientTimeout(total=10))  # seconds
 config.init("HTTP_RAISE_FOR_STATUS", bool_like, True)
 
@@ -27,15 +29,8 @@ config.init("TELEGRAM_URL", str, "https://api.telegram.org/bot{token}/sendMessag
 config.init("TELEGRAM_CHAT_ID", int, None)
 config.init("TELEGRAM_TEXT_TEMPLATE", str, "{flight.title}\n{flight.link}")
 
-config.init("PILOT_USERNAMES", lambda val: val.split(","), {})
-config.init("SLEEP", int, 10)  # seconds
-config.init("BACKOFF_SLEEP", int, 30)  # seconds
-
-if not config["WEBHOOK_URL"]:
-    logging.warning("Missing WEBHOOK_URL, webhook output turned off.")
-
-if not config["TELEGRAM_BOT_TOKEN"] or not config["TELEGRAM_CHAT_ID"]:
-    logging.warning("Missing TELEGRAM_BOT_TOKEN or TELEGRAM_CHAT_ID, Telegram output turned off.")
+config.init("SLEEP", int, 600)  # seconds
+config.init("BACKOFF_SLEEP", int, 1200)  # seconds
 
 
 @dataclass(unsafe_hash=True)
@@ -55,21 +50,30 @@ class Flight(object):
         return self.title.split("]")[-1].strip()
 
 
-class History(object):
+class History(MutableSet):
     """
     Stateful object to determine which flights have already been posted.
     """
+
+    def discard(self, flight):
+        del self._flights[flight]
+
+    def __len__(self):
+        return len(self._flights)
+
+    def __iter__(self):
+        return iter(self._flights.keys())
 
     def __init__(self):
         # key = flight, value = "is touched in current round"
         self._flights = {}
 
-    def should_skip(self, flight):
+    def __contains__(self, flight):
         if flight in self._flights:
             self._flights[flight] = True
         return flight in self._flights
 
-    def track(self, flight):
+    def add(self, flight):
         # just touch
         self._flights[flight] = True
 
@@ -127,7 +131,19 @@ async def do_output(session, flight):
 
 
 async def main():
-    history = History()
+    if not config["FEED_URL"]:
+        logging.error("Missing FEED_URL, exiting.")
+        sys.exit(1)
+
+    if not config["WEBHOOK_URL"]:
+        logging.warning("Missing WEBHOOK_URL, webhook output turned off.")
+
+    if not config["TELEGRAM_BOT_TOKEN"] or not config["TELEGRAM_CHAT_ID"]:
+        logging.warning("Missing TELEGRAM_BOT_TOKEN or TELEGRAM_CHAT_ID, Telegram output turned off.")
+
+    already_posted = History()
+    startup = True
+
     async with ClientSession(**config.get_namespace("HTTP_")) as session:
         while True:
             try:
@@ -139,21 +155,27 @@ async def main():
                 await asyncio.sleep(config["BACKOFF_SLEEP"])
                 continue
 
-            history.next_round()
+            already_posted.next_round()
 
             flights = parse_feed(feed)
             logging.info(f"Parsed: {len(flights)} flights")
 
-            flights = [f for f in flights if f.pilot_username in config["PILOT_USERNAMES"]]
-            logging.info(f"Filter by pilot: {len(flights)} flights")
-
-            flights = [f for f in flights if not history.should_skip(f)]
-            logging.info(f"Filter already posted: {len(flights)} flights")
+            if startup:
+                startup = False
+                # first round - assume that everything we parse is already posted
+                for flight in flights:
+                    already_posted.add(flight)
+                logging.info(f"Sleeping for {config['SLEEP']} seconds")
+                await asyncio.sleep(config["SLEEP"])
+                continue
 
             for flight in flights:
+                if flight in already_posted:
+                    # this check "touches" the history (= informs the object, that the flight is still active and should not be purged)
+                    continue
                 try:
                     await do_output(session, flight)
-                    history.track(flight)
+                    already_posted.add(flight)
                     logging.info(f"Posted: {flight}")
                 except (ClientError, asyncio.TimeoutError):
                     logging.exception("Post failed")
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -6,14 +6,25 @@ from monitor import History, Flight
 def test_history():
     flight = Flight("a", "b", datetime.datetime.now())
     h = History()
+
     h.next_round()
-    assert not h.should_skip(flight)
-    h.track(flight)
-    assert h.should_skip(flight)
+    assert flight not in h
+
     h.next_round()
-    assert h.should_skip(flight)
+    h.add(flight)
+    assert flight in h
+
     h.next_round()
-    assert h.should_skip(flight)
+    h.add(flight)
+    assert flight in h
+
     h.next_round()
+    h.add(flight)
+    assert flight in h
+
+    h.next_round()
+    # assert flight in h
+    # ^^^ do NOT run this assert, because it triggers the "touch"
+
     h.next_round()  # now the history state for `flight` should clear
-    assert not h.should_skip(flight)
+    assert flight not in h
